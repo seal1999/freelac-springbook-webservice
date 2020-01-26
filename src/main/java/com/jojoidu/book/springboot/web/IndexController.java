87b09bfc6856e8e49c5baf6f4f5b37e70badbfc0
Seungkwon Kim <teacher.seal@gmail.com>
2020-01-26 23:50:08 +0900

    Chapter 4. completed

--- src/main/java/com/jojoidu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoidu/book/springboot/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.jojoidu.book.springboot.web;
 
+import com.jojoidu.book.springboot.config.auth.LoginUser;
+import com.jojoidu.book.springboot.config.auth.dto.SessionUser;
+import com.jojoidu.book.springboot.domain.user.User;
 import com.jojoidu.book.springboot.service.PostsService;
 import com.jojoidu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +20,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
